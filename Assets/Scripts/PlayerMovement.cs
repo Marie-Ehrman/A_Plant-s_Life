4a75b82482b982ae844cdf96aa5ec02e9bfb012f
Marie-Ehrman <marie.e.ehrman@gmail.com>
2021-11-11 13:23:47 -0800

    Movement and Jump works!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,31 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-    public bool isGrounded;
+
 
     public float speed = 12f;
     public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    public float jumpHeight = 0.1f;
+    public float groundDistance;
+    public LayerMask groundMask;
+   
 
     Vector3 velocity;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
+       
         if (isGrounded && velocity.y < 0) {
-            velocity.y = -2f;
+
+            velocity.y = -8f;
         }
 
 
@@ -42,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && isGrounded) {
             Debug.Log("JUMP!");
-            //velocity.y = Mathf.Sqrt(jumpHeight) * -2f * gravity;
+            velocity.y = Mathf.Sqrt(jumpHeight) * -2f * gravity;
         }
 
         velocity.y += gravity * Time.deltaTime;
