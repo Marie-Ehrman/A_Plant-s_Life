ac5e1a00a58339fafebb5fe78c18962bd5390952
Marie-Ehrman <marie.e.ehrman@gmail.com>
2021-11-11 11:30:11 -0800

    add project files to repo

---
+++ Assets/Scripts/PlatformerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerController : MonoBehaviour
+{
+
+    //Speed of character movement and height of the jump. Set these values in the inspector.
+    public float speed;
+    public float jumpHeight;
+    public AudioSource jumpSound;
+
+    //Assigning a variable where we'll store the Rigidbody2D component.
+    private Rigidbody rb;
+
+    private bool onGround;
+    private bool canJump;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Sets our variable 'rb' to the Rigidbody2D component on the game object where this script is attached.
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if the player is on the ground. If we are, then we are able to jump.
+        if (onGround == true)
+        {
+            canJump = true;
+        }
+        //If we're able to jump and the player has pressed the space bar, then we jump!
+        if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
+        {
+            jumpSound.Play();
+            Debug.Log("jump");
+            rb.velocity = Vector3.up * jumpHeight;
+        }
+
+
+        //Movement code for left and right arrow keys.
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity =  Vector3.left;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity =  Vector3.right;
+        }
+        //ELSE if we're not pressing an arrow key, our velocity is 0 along the X axis, and whatever the Y velocity is (determined by jump)
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //If we collide with an object tagged "ground" then our jump resets and we can now jump.
+        if (collision.gameObject.tag == "ground")
+        {
+            onGround = true;
+            print(onGround);
+            //print statements print to the Console panel in Unity. 
+            //This will print the value of onGround, which is a boolean, so either True or False.
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //If we exit our collision with the "ground" object, then we are unable to jump.
+        if (collision.gameObject.tag == "ground")
+        {
+            onGround = false;
+            print(onGround);
+        }
+    }
+}
